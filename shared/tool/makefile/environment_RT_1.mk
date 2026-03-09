7977d9ef43b2bf27813e2ecb855216bb8708ef79
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2026-03-09 04:05:09 +0000

    doc tweaks, make work

---
+++ shared/tool/makefile/environment_RT_1.mk
@@ -0,0 +1,30 @@
+# enviroment_RT_1.mk
+# makefile environment variable defaults.
+# cc is the name of the C compiler, a file called <name>.c is C source code.
+# RT uses header integrated C source files, i.e. the source and the header are the same file
+
+SHELL=/bin/bash
+
+# environment_RT_1.mk
+
+ECHO := printf "%b\n"
+
+C_SOURCE_DIR     := authored
+C                := gcc
+CFLAGS           := -std=gnu11 -Wall -Wextra -Wpedantic -finput-charset=UTF-8
+CFLAGS           += -MMD -MP
+CFLAGS           += -I $(C_SOURCE_DIR)
+
+LIBRARY_NAME    := $(PROJECT)
+LIBRARY_NAME    := $(subst -,_,$(LIBRARY_NAME))
+
+LIBRARY_DIR     := scratchpad
+LIBRARY_FILE    := $(LIBRARY_DIR)/lib$(LIBRARY_NAME).a
+
+LN_FLAGS        := -L$(LIBRARY_DIR) -L/lib64 -L/lib
+
+MACHINE_DIR     := scratchpad/made
+
+KMOD_SOURCE_DIR  := cc
+KMOD_CCFLAGS    := -I $(KMOD_SOURCE_DIR)
+KMOD_OUTPUT_DIR := scratchpad/kmod
