176747fd65c0408d188227cbe1c26e117b829576
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2026-03-10 09:12:17 +0000

    updating example with namespace

---
+++ developer/authored/Greeter/Math.lib.c
@@ -0,0 +1,17 @@
+#ifndef ExampleGreet·math·ONCE
+#define ExampleGreet·math·ONCE
+
+#ifndef ExampleGreet·math
+  #define ExampleGreet·math
+#endif
+
+int ExampleGreet·Math·add(int a ,int b);
+
+#ifdef ExampleGreet·math
+
+int ExampleGreet·Math·add(int a ,int b){
+  return a + b;
+}
+
+#endif // ExampleGreet·math
+#endif // ExampleGreet·math·ONCE
