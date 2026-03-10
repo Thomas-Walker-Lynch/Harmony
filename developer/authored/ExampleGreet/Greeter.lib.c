e4665fd73027514ace4a07a132e8ebb5540c5990
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2026-03-10 17:40:06 +0000

    a working token based RT code format formatter

--- developer/authored/ExampleGreet/Greeter.lib.c
+++ developer/authored/ExampleGreet/Greeter.lib.c
@@ -8,12 +8,13 @@ void ExampleGreet·Greeter·hello_loop(int count);
 #ifdef ExampleGreet·Greeter
   #include <stdio.h>
 
-  void ExampleGreet·Greeter·hello_loop(int count){
+  void ExampleGreet·Greeter·hello_loop(int count){ 
     for(int TM = 0; TM < count; ++TM){
       int current_count = ExampleGreet·Math·add(TM ,1);
       printf("Hello iteration: %d\n" ,current_count);
     }
   }
+
 #endif // ExampleGreet·Greeter
 
 #endif // ExampleGreet·Greeter·ONCE
