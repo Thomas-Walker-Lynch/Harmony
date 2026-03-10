e97fe422d7976a52983448685da06ed72db3f042
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2026-03-10 11:34:51 +0000

    working ExampleGreet with implementation gate

--- developer/authored/ExampleGreet/Math.lib.c
+++ developer/authored/ExampleGreet/Math.lib.c
@@ -1,17 +1,12 @@
 #ifndef ExampleGreet·Math·ONCE
 #define ExampleGreet·Math·ONCE
 
-#ifndef ExampleGreet·Math
-  #define ExampleGreet·Math
-#endif
-
 int ExampleGreet·Math·add(int a ,int b);
 
 #ifdef ExampleGreet·Math
-
-int ExampleGreet·Math·add(int a ,int b){
-  return a + b;
-}
-
+  int ExampleGreet·Math·add(int a ,int b){
+    return a + b;
+  }
 #endif // ExampleGreet·Math
+
 #endif // ExampleGreet·Math·ONCE
