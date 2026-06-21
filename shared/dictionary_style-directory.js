202823d848257b0b8a48d6c9f24065fd305cc1b2
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2026-06-21 13:40:07 +0000

    refined developer/tool/make

--- shared/dictionary_style-directory.js
+++
@@ -1,4 +0,0 @@
-window.StyleRT_namespaces = {
-  "RT": window.RT_REPO_ROOT + "shared/linked-project/RT-style-JS_public/consumer/release/RT"
-  ,"Project": window.RT_REPO_ROOT + "shared/authored/style"
-};
